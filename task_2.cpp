47aca73c295d701e29959b8d33e45cc3cb972641
Ansa868 <ansashahid86@gmail.com>
2024-12-19 09:12:45 -0800

    Add files via upload

---
+++ task_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float Number1,Number2,Sum;
+    cout<<"Enter no 1:";
+    cin>>Number1;
+
+    cout<<"Enter no 2:";
+    cin>>Number2;
+
+    cout<<"Sum is:" <<Number1+Number2;
+    return 0;
+}
