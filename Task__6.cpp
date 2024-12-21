bc7e6e04bc869c624bc4ee8c2d222aa09628bc42
Ansa868 <ansashahid86@gmail.com>
2024-12-20 23:17:23 -0800

    Add files via upload

---
+++ Task__6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+ void sum()
+ {
+    int no1,no2;
+    cout<<"Enter no. 1:";
+  cin>>no1;
+ cout<<"Enter no.2:";
+ cin>>no2;
+ cout<<"Sum is:"<<no1+no2;
+ }
+int main(){
+   sum();
+   return 0;
+
+}
---
+++ Task__7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void multiplication(){
+     int no1,no2;
+     cout<<"Number 1:";
+     cin>>no1;
+
+     cout<<"Number 2:";
+     cin>>no2;
+
+     cout<<"Multiplication:"<<no1*no2;
+
+}
+   int main(){
+    multiplication();
+    return 0;
+   }
