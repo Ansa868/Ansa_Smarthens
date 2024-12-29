3809b8934cc169ec14a8e568e9c31d48fef5e250
Ansa868 <ansashahid86@gmail.com>
2024-12-28 23:02:02 -0800

    Add files via upload

---
+++ Fuel.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void distance(){
+    float distance;
+    cout<<"Distance: ";
+    cin>>distance;
+    cout<<"Calculated Fuel: "<<distance*10;
+    
+}
+int main(){
+
+    distance();
+    return 0;
+}
