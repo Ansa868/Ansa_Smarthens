67ef1d0b39c89d114e2038cba29378c743cd419f
Ansa868 <ansashahid86@gmail.com>
2025-01-03 22:54:36 -0800

    Add files via upload

---
+++ Week_1/Calculator.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void sum(double no1,double no2){
+cout<<"Enter no.1:";
+cin>>no1;
+cout<<"Enter no.2:";
+cin>>no2;
+cout<<"Sum: "<<no1+no2;
+}
+
+void subtract(double no1,double no2){
+cout<<"Enter no.1:";
+cin>>no1;
+cout<<"Enter no.2:";
+cin>>no2;
+cout<<"Subtract: "<<no1-no2;
+}
+
+void multiplication(double no1,double no2){
+cout<<"Enter no.1:";
+cin>>no1;
+cout<<"Enter no.2:";
+cin>>no2;
+cout<<"Multiplication: "<<no1*no2;
+}
+
+void Division(double no1,double no2){
+cout<<"Enter no.1:";
+cin>>no1;
+cout<<"Enter no.2:";
+cin>>no2;
+cout<<"Division: "<<no1/no2;
+}
+int main()
+{    int Choice(+,*,/,-)
+    cout<<"Enter your choice: ";
+    if(Choice==+){
+        sum();
+        }
+    }
+    
+
+
+    return 0;
