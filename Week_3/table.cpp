086254c43eeebe79a6f369023bc8375218a80e3c
Ansa868 <ansashahid86@gmail.com>
2025-01-04 03:56:16 -0800

    Add files via upload

---
+++ Week_3/table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int num=0;
+    cout<<"Enter Number= "<<endl;
+    cin>>num;
+    for(i=1;i<=10;i++){
+        cout<<num<<"*"<<i<<"="<<num*i<<endl;
+    }
+     return 0;
+    }
