4c80ca16821c6309407911aad66639178dec4c0d
Ansa868 <ansashahid86@gmail.com>
2025-02-06 19:28:03 +0500

    Add files via upload

---
+++ Week__6/Array_biginners/Task__6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sum=0;
+    float average=0;
+    int numbers[5]={1,2,3,4,5};
+    for (int i = 0; i < count; i++)
+    {
+        sum=sum + numbers[i];
+    }
+    average=sum/5;
+    cout<<"Sum="<<sum<<endl;
+    cout<<"Avg"<<average;
+}
