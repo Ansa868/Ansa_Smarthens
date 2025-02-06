daaeef4c18ab692b4f9552623602ee799523bf79
Ansa868 <ansashahid86@gmail.com>
2025-02-06 19:07:09 +0500

    Add files via upload

---
+++ Week__6/array bigginers/Task__2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[5];
+    cin>>numbers[0];
+    cin>>numbers[1];
+    cin>>numbers[2];
+    cin>>numbers[3];
+    cin>>numbers[4];
+    cout<<"The second element of the array="<<numbers[1];
+    cout<<"The forth element of the array="<<numbers[3];
+    return 0;
+}
---
+++ Week__6/array bigginers/Task__5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    
+        cout<<"Enter the size of array=";
+        cin>>n;
+        int number[n];
+        for (int i = 0; i < n; i++)
+        {
+            cout<<"The number in array ="<<number[i]<<endl;
+        }
+        
+    
+    
+    return 0;
+}
